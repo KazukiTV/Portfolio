dc5d49f09e2959c6281262eb7727157d38f10b82
Kazuki <99102354+Kazuki6891@users.noreply.github.com>
2023-04-16 21:53:02 +0300

    Add files via upload

---
+++ src/text/contentEN.js
@@ -0,0 +1,60 @@
+
+
+var contentEN = {
+  "aboutme" : {
+    "name" : "About me",
+    "content" : 
+<>
+<p>
+Hi 👋, I'm <a rel="noreferrer" target="_blank" href="https://github.com/Kazuki6891">Kazuki</a>,<br/>
+I am a self taught programmer who has a passion for development and stride for the best quality and efficient code provided to each client. I have 8 years total of professional experience(Earning Income) and 10 years total of experience in development(Learning and broadening my knowledge).
+</p>
+<p>
+Moreover, I like to always learn new things and develop new projects whether it be small and easy or large and complex. I love learning new languages and frameworks and will always find some way to advance my knowledge to the next level.
+</p>
+<p>
+I have developed personal projects in which I work with web apps, APIs, bots, and databases. I used concepts of functional programming, object-oriented programming, REST API, and image processing.
+</p>
+<p>
+See the section about my Development skills to see my projects and languages that I used, or reach out to me if you have a interest in working together.
+</p>
+</>
+  },
+
+
+  "skills" : {
+    "name" : "Skills", 
+    "content" : 
+<>
+<p>
+To work with image processing, bots, and automation I used Python. Here's one <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/telegram_ecommerce">bot</a> that I made, and a <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/Fractals-JuliaSet">script</a> where I use image processing to create a fractal.
+</p>
+<p>
+To develop web apps and APIs I used JavaScript, ReactJs, and ExpressJS. Here's a <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/DraggableDialog">library</a> that I developed with this language.
+</p>
+<p>
+Besides, I know and like many concepts of functional programming, where I learned from the Haskell language and javascript. See a <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/ValidationMethods">package</a> where I used the beauty of the Haskell type system.
+</p>
+<p>
+See my <a rel="noreferrer" target="_blank" href="https://github.com/Kazuki6891">github account</a> to known more about these projects.
+</p>
+</>
+  },
+
+
+  "contact" : {
+    "name" : "Contact",
+    "content" : 
+<>
+<p>I'm open to work on private or open-source projects, or only talk with you, so feel free to send me a message, I always like to make new connections.</p>
+<p>Here my contact info:</p>
+<p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/Kazuki6891">My Github</a></p>
+<p> 👔 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/AlexanderMorales/">My Linkedin</a></p>
+<p> 📞 <a rel="noreferrer" target="_blank" href="Kazuki#6891">My Discord</a></p>
+<p> 📨 <a href="mailto:kazukicommissions@gmail.com">kazukicommissions@gmail.com</a>My Email</p>
+</>
+  }
+}
+
+
+export default contentEN
