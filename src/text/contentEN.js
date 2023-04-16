b04a1723fceea8179ef245cde9e85de177425cd0
Kazuki <99102354+Kazuki6891@users.noreply.github.com>
2023-04-16 22:02:55 +0300

    Update contentEN.js

--- src/text/contentEN.js
+++ src/text/contentEN.js
@@ -6,7 +6,7 @@ var contentEN = {
     "content" : 
 <>
 <p>
-Hi 👋, I'm <a rel="noreferrer" target="_blank" href="https://github.com/Kazuki6891">Kazuki</a>,<br/>
+Hi 👋, I'm <a rel="noreferrer" target="_blank" href="https://github.com/kazuki6891">Kazuki</a>,<br/>
 I am a self taught programmer who has a passion for development and stride for the best quality and efficient code provided to each client. I have 8 years total of professional experience(Earning Income) and 10 years total of experience in development(Learning and broadening my knowledge).
 </p>
 <p>
@@ -30,13 +30,13 @@ See the section about my Development skills to see my projects and languages tha
 To work with image processing, bots, and automation I used Python. Here's one <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/telegram_ecommerce">bot</a> that I made, and a <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/Fractals-JuliaSet">script</a> where I use image processing to create a fractal.
 </p>
 <p>
-To develop web apps and APIs I used JavaScript, ReactJs, and ExpressJS. Here's a <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/DraggableDialog">library</a> that I developed with this language.
+To develop web apps and APIs I used JavaScript, ReactJs, and ExpressJS. Here's a <a rel="noreferrer" target="_blank" href="https://github.com/kazuki6891/DraggableDialog">library</a> that I developed with this language.
 </p>
 <p>
 Besides, I know and like many concepts of functional programming, where I learned from the Haskell language and javascript. See a <a rel="noreferrer" target="_blank" href="https://github.com/raulpy271/ValidationMethods">package</a> where I used the beauty of the Haskell type system.
 </p>
 <p>
-See my <a rel="noreferrer" target="_blank" href="https://github.com/Kazuki6891">github account</a> to known more about these projects.
+See my <a rel="noreferrer" target="_blank" href="https://github.com/kazuki6891">github account</a> to known more about these projects.
 </p>
 </>
   },
@@ -48,7 +48,7 @@ See my <a rel="noreferrer" target="_blank" href="https://github.com/Kazuki6891">
 <>
 <p>I'm open to work on private or open-source projects, or only talk with you, so feel free to send me a message, I always like to make new connections.</p>
 <p>Here my contact info:</p>
-<p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/Kazuki6891">My Github</a></p>
+<p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/kazuki6891">My Github</a></p>
 <p> 👔 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/AlexanderMorales/">My Linkedin</a></p>
 <p> 📞 <a rel="noreferrer" target="_blank" href="Kazuki#6891">My Discord</a></p>
 <p> 📨 <a href="mailto:kazukicommissions@gmail.com">kazukicommissions@gmail.com</a>My Email</p>
